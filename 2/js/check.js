3678f0bb4f7c491c25991912e2793af793da117a
Keks <keks@htmlacademy.ru>
2016-05-27 11:24:42 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/check.js
+++ 2/js/check.js
@@ -1,4 +1,4 @@
-function getMessage((a:*, b:*=):string) {
+var getMessage = function(a, b) {
   if (typeof a === 'boolean') {
     if (a == true) {
     return('Переданное GIF-изображение анимировано и содержит' + ' ' + b + ' ' + 'кадров');
@@ -8,19 +8,21 @@ function getMessage((a:*, b:*=):string) {
     }
   }
   if (typeof a === 'number') {
-    return('Переданное SVG-изображение содержит' + ' ' + a + ' ' + 'объектов и' + ' ' + (b * 4) + ' ' + 'атрибутов');
+    return('Переданное SVG-изображение содержит' + ' ' + a + ' ' + 'объектов и' + ' ' + (b * 4) + ' ' + 'аттрибутов');
   }
   if (Array.isArray(a)) {
     var sum = 0;
-    for var i = 0; i < a.length; i++ {
-      sum += array[i];
+    for(var i = 0; i < a.length; i++){
+      sum += a[i];
     }
     return('Количество красных точек во всех строчках изображения:' + ' ' + sum);
   }
-  if ((Array.isArray(a) && Array.isArray(b)) {
+  if (Array.isArray(a) && Array.isArray(b)) {
     var square = 0;
-    for var i = 0; i < a.length; i++ {
-      square += a[i] * b[i];
+    var arr = [];
+    for(var i = 0; i < a.length; i++){
+      arr.push(a[i] * b[i]);
+      square += arr[i];
     }
     return('Общая площадь артефактов сжатия:' + ' ' + square + ' ' + 'пикселей');
   }
